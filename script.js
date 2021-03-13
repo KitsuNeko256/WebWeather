300d6c069940ce00a2a2adf1f587bf1c4a32763f
Kitsune <44873289+KitsuNeko256@users.noreply.github.com>
2021-03-13 23:01:03 +0700

    Add and delete city; Load favs Enter works on submit Offline message

--- script.js
+++ script.js
@@ -1,54 +1,105 @@
-﻿function contactsPopup() {
-	var popup = document.getElementById("contactsPopup");
-	popup.classList.toggle("show");
+﻿
+function deleteCity(element, cityID) {
+	window.localStorage.removeItem(cityID);
+	element.parentElement.parentElement.remove();
 }
 
-function picSwap() {
-	var picList = ["pic1.png", "pic2.jpg", "pic3.jpg", "pic1.png"];
-	var pic = document.getElementById("picGallery");
-	var name = pic.src;
-	name = name.substring(name.length - 8, name.length);
-	for (i = 0; i < 3; i++) {
-		if(name == picList[i]) {
-			pic.src = picList[i+1];
+function addCity(searchType, newCity, searchValue = document.getElementById('favInputName').value) {
+    let xhr = new XMLHttpRequest();
+	if (searchType == "name") {
+		searchValue = searchValue.toLowerCase();
+		xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=395cea13dd1f22db118d27c7297923c3&units=metric`);
+    } else if (searchType == "coord") {
+		
+	} else if (searchType == "id") {
+		xhr.open('GET', `https://api.openweathermap.org/data/2.5/weather?id=${searchValue}&appid=395cea13dd1f22db118d27c7297923c3&units=metric`);
+	}
+	   
+	xhr.send();
+    
+    xhr.onload = function() {
+      if (xhr.status != 200) {
+        alert(`Город не найден!`);
+      } else {
+        var cityData = JSON.parse(xhr.response);
+		var cityName = cityData.name;
+        if (newCity && window.localStorage.getItem(cityData.id) !== null) {
+			alert(`Город ${cityName} уже есть в списке!`);
+			return;
 		}
+		var template = document.getElementById("favCityTemplate");
+		var	clone = template.content.cloneNode(true);
+		var city = clone.children[0];
+		var header = city.children[0];
+		header.children[0].innerHTML = cityName;
+		header.children[1].src = `https://openweathermap.org/img/w/${cityData.weather[0].icon}.png`;
+		header.children[2].innerHTML =  `${cityData.main.temp}°C`;
+		header.children[3].setAttribute('onclick',`deleteCity(this, ${cityData.id})`);
+
+		var list = city.children[1].children;
+		list[0].innerHTML = `<b>Ветер</b> ${cityData.wind.speed} м/с`;
+		list[1].innerHTML = `<b>Облачность</b> ${cityData.weather[0].description}`;
+		cityData.main.pressure *= 0.75;
+		list[2].innerHTML = `<b>Давление</b> ${cityData.main.pressure} мм р.с.`;
+		list[3].innerHTML = `<b>Влажность</b> ${cityData.main.humidity} %`;
+		list[4].innerHTML = `<b>Координаты</b> [${cityData.coord.lon}, ${cityData.coord.lat}]`;
+
+
+		document.getElementById("favList").appendChild(city);
+
+		if (window.localStorage.getItem(cityData.id) === null) {
+			window.localStorage.setItem(cityData.id, cityData.id);
+		}
+      }
+    };
+
+	if(newCity)
+		document.getElementById('favInputName').value = '';
+}
+
+function initFavCityInput() {
+	const input = document.getElementById("favInputName");
+
+	input.addEventListener("keyup", function(event) {
+		if (event.keyCode === 13) {
+			event.preventDefault();
+			document.getElementById("favInputButton").click();
+		}
+	});
+}
+
+function initOffline() {
+	window.addEventListener("offline", function (e) {
+		alert("Соединение потеряно, перезагрузите страниицу!");
+	});
+}
+
+function loadFavCities() {
+	for (var i = 0; i < window.localStorage.length; i++) {
+		console.log(window.localStorage.key(i));
+		addCity("id", false, window.localStorage.key(i));
 	}
 }
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+
+
+function init() {
+	initOffline();
+	initFavCityInput();
+	loadFavCities();
 }
 
-function deleteMessage(e) {
-	e.target.parentNode.remove();
+
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+		var t = navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+		alert("Geolocation is not supported by this browser.");
+    }
 }
-
-function chatMessage() {
-	var chatMessageBox = document.getElementById("chatMessageBox");
-
-	var message = document.createElement("div");
-	message.classList.toggle("chatMessage");
-	message.classList.toggle("chatMessage1");
-	var img1 = document.createElement("img");
-	img1.classList.toggle("chatCross");
-	img1.src = "cross.png";
-	img1.onclick = deleteMessage;
-	message.appendChild(img1);
-	message.appendChild(document.createTextNode(document.getElementById("chatText").value));
-	chatMessageBox.append(message);
-
-	var adj = ["Белая", "Серая", "Черная"];
-	var noun = ["лисичка", "кошечка", "поняша"];
-	var verb = ["стоит", "сидит", "лежит"];
-	var text = adj[getRandomInt(3)] + " " + noun[getRandomInt(3)] + " " + verb[getRandomInt(3)];
-
-	var answerMessage = document.createElement("div");
-	answerMessage.classList.toggle("chatMessage");
-	answerMessage.classList.toggle("chatMessage2");	
-	var img2 = img1.cloneNode(true);
-	img2.onclick = deleteMessage;
-	answerMessage.appendChild(img2);
-	answerMessage.appendChild(document.createTextNode(text));
-	chatMessageBox.append(answerMessage);
-
-	chatMessageBox.scrollTop = chatMessageBox.scrollHeight;
+function showPosition(position) {
+    var elem = document.getElementById("demo");
+    elem.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
 }
